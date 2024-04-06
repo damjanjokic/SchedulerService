467b77b4ab0ec2b2939a6aabb3cc2a73a67ca8b3
Damjan Jokic <damjan.jokic@valcon.com>
2024-04-06 15:10:04 +0200

    Initial commit - Mongo and CRUD implemented

---
+++ SchedulerService/SchedulerService/Controllers/AppointmentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using SchedulerService.Data;
+using SchedulerService.Entities;
+
+namespace SchedulerService.Controllers;
+
+[Route(("api/[controller]"))]
+[ApiController]
+public class AppointmentController : ControllerBase
+{
+    private readonly IMongoCollection<Appointment> _appointments;
+    public AppointmentController(MongoDbService mongoDbService)
+    {
+        _appointments = mongoDbService.Database?.GetCollection<Appointment>("appointment");
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Appointment>> Get()
+    {
+        return await _appointments.Find(FilterDefinition<Appointment>.Empty).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Appointment>> GetById(ObjectId id)
+    {
+        var filter = Builders<Appointment>.Filter.Eq(x => x.Id, id);
+        var appointment = await _appointments.Find(filter).FirstOrDefaultAsync();
+        return appointment is not null ? Ok(appointment) : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(Appointment appointment)
+    {
+        appointment.Id = ObjectId.GenerateNewId();
+        await _appointments.InsertOneAsync(appointment);
+        return Ok(appointment.Id);
+    }
+    
+}
---
+++ SchedulerService/SchedulerService/Entities/Appointment.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SchedulerService.Entities;
+
+public class Appointment
+{
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+    [BsonElement("organisation_id"), BsonRepresentation(BsonType.Binary)]
+    public Guid OrganisationId { get; set; }
+    [BsonElement("date"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime Date { get; set; }
+    [BsonElement("start_time"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime StartTime { get; set; }
+    [BsonElement("end_time"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime EndTime { get; set; }
+    [BsonElement("description"), BsonRepresentation(BsonType.String)]
+    public string Description { get; set; }
+    [BsonElement("organiser_id"), BsonRepresentation(BsonType.Binary)]
+    public Guid OrganiserId { get; set; }
+    [BsonElement("organiser_approval_pending"), BsonRepresentation(BsonType.Boolean)]
+    public bool OrganiserApprovalPending { get; set; }
+    [BsonElement("set"), BsonRepresentation(BsonType.Boolean)]
+    public bool Set { get; set; }
+    [BsonElement("attendees")]
+    public List<Guid> Attendees { get; set; }
+}
